6e7a573ea2020aa25d60da83a0f264abe67a11a0
Shahdad Peykani <115485786+shahdadpeykani@users.noreply.github.com>
2024-08-14 22:36:04 +0300

    Update main.py
    I decided to improve this code after learning the Error Handling and Exception topic. now, in case of any invalid input, it does not give any error and keeps working.

--- NATO-Alphabet-Project/main.py
+++ NATO-Alphabet-Project/main.py
@@ -4,8 +4,19 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
 
-output = [phonetic_dic[letter] for letter in word]
+def generate_phonetic():
 
-print(output)
+    word = input("Enter a word: ").upper()
+
+    try:
+        output = [phonetic_dic[letter] for letter in word]
+
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+        generate_phonetic()
+    else:
+        print(output)
+
+
+generate_phonetic()
